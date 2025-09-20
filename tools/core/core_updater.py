32f02109792a869bb6262c531e5cef4b56e9293b
Deploy Manager <deploy@example.com>
2025-09-21 00:46:37 +0300

    🚀 Deploy 5.1.10 - 2025-09-21

--- tools/core/core_updater.py
+++ tools/core/core_updater.py
@@ -643,10 +643,9 @@ class DockerManager:
             self.messages.print_output(f"{Colors.CYAN}💡 Собираем Docker образ...{Colors.END}\n")
             
             return_code = self.utils._run_with_progress_output(
-                ['docker', 'build', '--no-cache', '--progress=plain', '-t', 'coreness-image', '-f', 'Dockerfile', '..'], 
+                ['docker', 'build', '-t', 'coreness-image', '-f', 'Dockerfile', '..'], 
                 "Сборка Docker образа",
-                cwd=docker_dir,
-                buffer_size=40  # Увеличиваем буфер для более подробных логов
+                cwd=docker_dir
             )
             if return_code != 0:
                 self.messages.print_output(f"{Colors.RED}❌ Ошибка сборки Docker образа{Colors.END}\n")
